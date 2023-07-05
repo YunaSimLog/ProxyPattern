dd8e23c59a4dee144a6f87ffb262c521364c4370
yunasim <ynsim@solidit.co.kr>
2023-07-05 23:32:21 +0900

    #✨feature: 프록시 디자인 패턴 구현

--- ProxyPattern/Program.cs
+++ ProxyPattern/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace ProxyPattern
     {
         static void Main(string[] args)
         {
+            Client client = new Client();
+
+            Console.WriteLine("Client: real Subject로 클라이언트 코드 실행");
+            RealSubject realsubject = new RealSubject();
+            client.ClientCode(realsubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: 프록시로 동일한 클라이언트 코드 실행");
+            Proxy proxy = new Proxy(realsubject);
+            client.ClientCode(proxy);
         }
     }
 }
---
+++ ProxyPattern/ProxyPattern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public interface ISubject
+    {
+        void Request();
+    }
+
+    class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("RealSubject: Handling Request");
+        }
+    }
+
+    class Proxy : ISubject
+    {
+        private RealSubject _realsubject;
+
+        public Proxy(RealSubject realsubject)
+        {
+            _realsubject = realsubject;
+        }
+
+        public bool CheckAccess()
+        {
+            Console.WriteLine("Proxy: 실제 요청을 실행하기 전에 액세스를 확인");
+            return true;
+        }
+
+        public void LogAccess()
+        {
+            Console.WriteLine("Proxy: 요청 시간 기록");
+        }
+
+        public void Request()
+        {
+            if(CheckAccess())
+            {
+                _realsubject.Request();
+                LogAccess();
+            }
+        }
+    }
+
+    public class Client
+    {
+        public void ClientCode(ISubject subject)
+        {
+            subject.Request();
+        }
+    }
+}
